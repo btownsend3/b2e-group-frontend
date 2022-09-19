8ff51c2dc26e0162b40536b7eef1266cd2dec0a8
yorkmacbook085 <bctownsend3@gmail.com>
2022-09-19 10:38:04 -0400

    Minor style changes

--- client/src/components/AssignmentList.js
+++ client/src/components/AssignmentList.js
@@ -13,15 +13,31 @@ function AssignmentList() {
 
     const quizMap = quizzes?.map((quiz, index) => {
         let grade = allQuizzes.find(item => item.id == quiz.id).responses.find(item => item.username == username).grade
-        return (
-            <Card key={index} className={'shadow p-3 m-4 bg-white rounded'} style={{width: "18rem"}}>
-                <Card.Title>{quiz.title}</Card.Title>
-                <Card.Text>{quiz.description}</Card.Text>
-                <Card.Text>{quiz.questions.length} questions</Card.Text>
-                { grade && <Card.Text>Grade: {grade}/{quiz.questions.length}</Card.Text> }
-                { !grade && permissionLevel > 0 && <TakeQuiz quiz={quiz}/>}
-            </Card>
-        )
+        if (!grade) {
+            return (
+                <Card key={index} className={'shadow p-3 m-4 bg-white rounded'} style={{width: "18rem"}}>
+                    <Card.Title>{quiz.title}</Card.Title>
+                    <Card.Text>{quiz.description}</Card.Text>
+                    <Card.Text>{quiz.questions.length} questions</Card.Text>
+                    { permissionLevel > 0 && <TakeQuiz quiz={quiz}/>}
+                </Card>
+            )
+        }
+    })
+
+    const gradedQuizMap = quizzes?.map((quiz, index) => {
+        let grade = allQuizzes.find(item => item.id == quiz.id).responses.find(item => item.username == username).grade
+        if (grade) {
+            return (
+                <Card key={index} className={'shadow p-3 m-4 bg-white rounded'} style={{width: "18rem"}}>
+                    <Card.Title>{quiz.title}</Card.Title>
+                    <Card.Text>{quiz.description}</Card.Text>
+                    <Card.Text>{quiz.questions.length} questions</Card.Text>
+                    { grade && <Card.Text>Grade: {grade}/{quiz.questions.length}</Card.Text> }
+                    { !grade && permissionLevel > 0 && <TakeQuiz quiz={quiz}/>}
+                </Card>
+            )
+        }
     })
 
     if (quizMap.length === 0) {
@@ -31,9 +47,20 @@ function AssignmentList() {
     } else {
         return (
             <>
-                <h3 className={'text-center'}>Assignments</h3>
-                <div className={'d-flex flex-wrap'}>
-                    {quizMap}
+                <div className={'d-flex justify-content-around my-4'}>
+                    <div className={'w-45'}>
+                        <h3 className={'text-center'}>Assignments</h3>
+                        <div className={'d-flex flex-wrap'}>
+                            {quizMap}
+                        </div>
+                    </div>
+                    <div className={'assignments-border'}/>
+                    <div className={'w-45'}>
+                        <h3 className={'text-center'}>Grades</h3>
+                        <div className={'d-flex flex-wrap'}>
+                            {gradedQuizMap}
+                        </div>
+                    </div>
                 </div>
                 <hr />
             </>
